2ea04f08ef7362f91402b9d03eb0e8b91610174d
Chimenka Goodluck <chimenkagoodluck809@gmail.com>
2025-09-18 15:13:57 +0100

    Update url.py
    I added the get_process route and uncommented the analyze function

--- backend/evidencemodule/url.py
+++ backend/evidencemodule/url.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('validate/', views.validate_field, name='validate-field'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    # path('analyze/', views.analyze_evidence, name='analyze_evidence'),
+    path('analyze/', views.analyze_evidence, name='analyze_evidence'),
+    path("get_process/", views.get_process, name="get_process")
    
 ]
 
