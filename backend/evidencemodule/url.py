d412960b186d9b67696919fe9a72eb394750ef8a
praiseolotu <olioxxayo@gmail.com>
2025-10-02 15:36:55 +0100

    clean url.py

--- backend/evidencemodule/url.py
+++ backend/evidencemodule/url.py
@@ -1,27 +1,8 @@
-<<<<<<< HEAD
-from django.urls import path
-from . import views
-
-=======
-# from django.urls import path, include
-# from . import views
-# urlpatterns = [
-#     path('', views.index, name='index'),
-#     path('validate/', views.validate_field, name='validate-field'),
-#     path('logout/', views.logout_view, name='logout'),
-#     path('dashboard/', views.dashboard, name='dashboard'),
-#     path('analyze/', views.analyze_evidence, name='analyze_evidence'),
-#     path("get_process/", views.get_process, name="get_process")  
-#     path('process_metrics/', views.process_metrics_view, name='process_metrics'),
-#     path("get_enriched/", views.get_enriched, name="get_enriched"),
-# ]
-
-
 from django.urls import path, include
 from django.conf import settings
 from . import views
 from django.conf.urls.static import static
->>>>>>> 50f3def3ffbe7ea0a92495596e971010644d655a
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('validate/', views.validate_field, name='validate-field'),
